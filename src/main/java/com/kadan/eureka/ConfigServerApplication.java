61ece35dbaa1e95b4b293f433f38b8fb3aa57d0e
kadan.li <kadan.li@bamboonetworks.com>
2018-08-27 21:47:48 +0800

    first commit by kadan

---
+++ src/main/java/com/kadan/eureka/ConfigServerApplication.java
@@ -0,0 +1,18 @@
+package com.kadan.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+//import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+//@EnableEurekaServer
+//@EnableDiscoveryClient
+@EnableConfigServer
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+}
